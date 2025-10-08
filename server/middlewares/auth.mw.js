af22522d5bbecbc4b40096e4fc7b8fc380714336
Nazar <160216692+daki1337@users.noreply.github.com>
2025-10-08 15:18:40 +0300

    update

--- server/middlewares/auth.mw.js
+++ server/middlewares/auth.mw.js
@@ -1,9 +1,39 @@
-const jwt = require("jsonwebtoken");
-const createError = require("http-errors");
 const CONSTANTS = require("../constants");
 const User = require("../models/User");
 const Order = require("../models/Order");
+const createError = require("http-errors");
+const jwt = require("jsonwebtoken");
 
+module.exports.isOwner = async (req, res, next) => {
+  if (req.params.idUser === req.user._id.toString()) {
+    return next();
+  }
+  next(createError(403, "Only owner"));
+};
+
+module.exports.isAdmin = async (req, res, next) => {
+  if (req.user?.role === "admin") {
+    return next();
+  }
+  next(createError(403, "Only admin"));
+};
+module.exports.canUpdateOrderStatus = async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.orderId);
+    if (!order) {
+      return next(createError(404, "Order not found"));
+    }
+    if (
+      req.user?.role === "admin" ||
+      req.user?._id.toString() === order?.user.toString()
+    ) {
+      return next();
+    }
+    next(createError(403, "You do not have permission"));
+  } catch (error) {
+    next(error);
+  }
+};
 module.exports.auth = async (req, res, next) => {
   try {
     const rowAuthorization = req.headers.authorization;
@@ -21,37 +51,5 @@ module.exports.auth = async (req, res, next) => {
   } catch (error) {
     console.log("auth error --->>>", error);
     next(createError(401, "Unautorized"));
-  }
-};
-
-module.exports.isAdmin = async (req, res, next) => {
-  if (req.user?.role === "admin") {
-    return next();
-  }
-  next(createError(403, "Only admin"));
-};
-
-module.exports.isOwner = async (req, res, next) => {
-  if (req.params.idUser === req.user._id.toString()) {
-    return next();
-  }
-  next(createError(403, "Only owner"));
-};
-
-module.exports.canUpdateOrderStatus = async (req, res, next) => {
-  try {
-    const order = await Order.findById(req.params.orderId);
-    if (!order) {
-      return next(createError(404, "Order not found"));
-    }
-    if (
-      req.user?.role === "admin" ||
-      req.user?._id.toString() === order?.user.toString()
-    ) {
-      return next();
-    }
-    next(createError(403, "You do not have permission"));
-  } catch (error) {
-    next(error);
   }
 };
