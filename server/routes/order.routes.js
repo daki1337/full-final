5a635016bf2bbff5aa6d69d58b3ee92daf3d35cf
Nazar <160216692+daki1337@users.noreply.github.com>
2025-10-08 15:27:08 +0300

    commit

--- server/routes/order.routes.js
+++ server/routes/order.routes.js
@@ -1,31 +1,31 @@
 const express = require("express");
-const {
-  auth,
-  isAdmin,
-  canUpdateOrderStatus,
-} = require("../middlewares/auth.mw");
-const { validate } = require("../middlewares/validate.mw");
-const {
-  createOrderSchema,
-  updateStatusOrderSchema,
-} = require("../validators/order.validator");
-const {
-  createOrder,
-  getAllOrders,
-  getAccountOrders,
-  getOrder,
-  updateStatusOrder,
-  createCheckoutSession,
-} = require("../controllers/order.controller");
 const { paginate } = require("../middlewares/pagination.mw");
 const { filterOrders } = require("../middlewares/filter.mw");
+const { validate } = require("../middlewares/validate.mw");
+const { 
+  auth, 
+  isAdmin, 
+  canUpdateOrderStatus 
+} = require("../middlewares/auth.mw");
+const { 
+  createOrderSchema, 
+  updateStatusOrderSchema 
+} = require("../validators/order.validator");
+const { 
+  createOrder, 
+  getAllOrders, 
+  getAccountOrders, 
+  getOrder, 
+  updateStatusOrder, 
+  createCheckoutSession 
+} = require("../controllers/order.controller");
 
 const router = express.Router();
 
 router.post("/", auth, validate(createOrderSchema), createOrder);
-
-router.get("/", auth, isAdmin, paginate, filterOrders, getAllOrders);
+router.post("/create-checkout-session", createCheckoutSession);
 router.get("/account", auth, paginate, getAccountOrders);
+router.get("/", auth, isAdmin, paginate, filterOrders, getAllOrders);
 router.get("/:orderId", auth, getOrder);
 router.patch(
   "/:orderId",
@@ -34,6 +34,5 @@ router.patch(
   validate(updateStatusOrderSchema),
   updateStatusOrder
 );
-router.post("/create-checkout-session", createCheckoutSession);
 
 module.exports = router;
