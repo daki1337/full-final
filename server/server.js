3e19d51d1b363177fb218bad94019371e15415ad
Nazar <160216692+daki1337@users.noreply.github.com>
2025-10-08 15:14:54 +0300

    update

--- server/server.js
+++ server/server.js
@@ -1,13 +1,9 @@
-const http = require('http');
 const connectDB = require('./config/db');
 const app = require('./app');
-
+const http = require('http');
 connectDB();
-
-const server = http.createServer(app);
-
 const port = 3000;
-
+const server = http.createServer(app);
 server.listen(port, () => {
   console.log('server started at port ', port);
 });
