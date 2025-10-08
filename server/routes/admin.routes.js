aead8757559dcb0b523df7da57ca9d2f72794efb
Nazar <160216692+daki1337@users.noreply.github.com>
2025-10-08 16:07:49 +0300

    Fix

--- server/routes/admin.routes.js
+++ server/routes/admin.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();|
+const router = express.Router();
 const { auth, isAdmin } = require("../middlewares/auth.mw");
 const { getStats } = require("../controllers/admin.controller");
 router.get("/stats", auth, isAdmin, getStats);
